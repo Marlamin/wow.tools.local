d670fdc75d581ca47acaa0dc900d1ae94d545990
Martin Benjamins <marlamin@marlamin.com>
2023-05-09 18:09:47 +0200

    Clear DB2 cache upon reloading hotfixes

--- Controllers/DBC/ReloadHotfixesController.cs
+++ Controllers/DBC/ReloadHotfixesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DBCD.Providers;
+using Microsoft.AspNetCore.Mvc;
 using wow.tools.local.Services;
+using wow.tools.Services;
 
 namespace wow.tools.local.Controllers
 {
@@ -7,10 +9,21 @@ namespace wow.tools.local.Controllers
     [ApiController]
     public class ReloadHotfixesController : ControllerBase
     {
+        private readonly DBDProvider dbdProvider;
+        private readonly DBCManager dbcManager;
+
+        public ReloadHotfixesController(IDBDProvider dbdProvider, IDBCManager dbcManager)
+        {
+            this.dbdProvider = dbdProvider as DBDProvider;
+            this.dbcManager = dbcManager as DBCManager;
+        }
+
         [HttpGet]
         public string Get()
         {
             HotfixManager.Clear();
+            dbcManager.ClearCache();
+            dbcManager.ClearHotfixCache();
             HotfixManager.LoadCaches();
             return "Reloaded hotfixes";
         }
