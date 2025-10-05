992ef7b78827051e7e294ce75c86fb3d88da21d8
Martin Benjamins <ictmartin@gmail.com>
2025-10-05 03:50:51 +0200

    Quest DB WIP stuff

--- Controllers/WDBController.cs
+++ Controllers/WDBController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using wow.tools.local.Services;
 using WoWFormatLib.FileReaders;
 
@@ -42,12 +43,52 @@ namespace wow.tools.local.Controllers
             return result;
         }
 
-        //[Route("quests")]
-        //[HttpGet]
-        //public string QuestDebug()
-        //{
-        //    //var questWDB = WDBReader.Read("C:\\World of Warcraft\\_retail_\\Cache\\WDB\\enUS\\questcache.wdb", "11.0.2.56196");
-        //    //return questWDB.ToString();
-        //}
+        [Route("quests")]
+        [HttpGet]
+        public DataTablesResult QuestTable(int draw, int start, int length)
+        {
+            //var questWDB = WDBReader.Read("C:\\World of Warcraft\\_retail_\\Cache\\WDB\\enUS\\questcache.wdb", "11.2.5.63534");
+            var questWDB = WDBReader.Read("C:\\World of Warcraft\\_beta_\\Cache\\WDB\\enUS\\questcache.wdb", "12.0.0.63534");
+
+            var result = new DataTablesResult()
+            {
+                draw = draw,
+                recordsTotal = questWDB.entries.Count,
+                recordsFiltered = questWDB.entries.Count,
+                data = []
+            };
+
+            if (length == -1)
+            {
+                start = 0;
+                length = 25;
+            }
+
+            var results = questWDB.entries.OrderBy(x => x.Key).Skip(start).Take(length);
+
+            foreach (var res in results)
+            {
+                result.data.Add(
+                [
+                    res.Key.ToString(), // ID
+                    res.Value["LogTitle"], // Filename 
+                ]);
+            }
+
+            return result;
+        }
+
+        [Route("quest")]
+        [HttpGet]
+        public string Quest(int id)
+        {
+            //var questWDB = WDBReader.Read("C:\\World of Warcraft\\_retail_\\Cache\\WDB\\enUS\\questcache.wdb", "11.2.5.63534");
+            var questWDB = WDBReader.Read("C:\\World of Warcraft\\_beta_\\Cache\\WDB\\enUS\\questcache.wdb", "12.0.0.63534");
+
+            if (id == 0)
+                return JsonSerializer.Serialize(questWDB.entries);
+            else
+                return JsonSerializer.Serialize(questWDB.entries.Where(x => x.Key == id));
+        }
     }
 }
